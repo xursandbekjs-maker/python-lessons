47c59c591ef4c891f0d52953a053735cf7cf82d6
xursandbekjs-maker <xursandbekjumaboyev987@gmail.com>
2026-03-17 16:26:19 +0500

    refactor: comment out example code in dictionary.py for clarity

--- dictionary.py
+++ dictionary.py
@@ -1,64 +1,69 @@
-# Data types (Ma'lumot turlari)
-# 1. integer; 2. float; 3. string; 4. boolean; 5. list; 6. tuple; 7. dictionary
-cars = ['audi', 'chevrolet', 'BYD', 'tesla'] 
+# # Data types (Ma'lumot turlari)
+# # 1. integer; 2. float; 3. string; 4. boolean; 5. list; 6. tuple; 7. dictionary
+# cars = ['audi', 'chevrolet', 'BYD', 'tesla'] 
 
-# Dictionary (lug'at)
-# key-value pair (kalit-qiymat juftligi)
-car = {
-    'brand': "Ford",
-    'name': "Mustang",
-    'year': 2000,
-    'color': 'blue'
-}
-print(car)
-print(type(car)) # <class 'dict'>
+# # Dictionary (lug'at)
+# # key-value pair (kalit-qiymat juftligi)
+# car = {
+#     'brand': "Ford",
+#     'name': "Mustang",
+#     'year': 2000,
+#     'color': 'blue'
+# }
+# print(car)
+# print(type(car)) # <class 'dict'>
 
-student = {
-    'fullname': "John Doe",
-    'age': 20,
-    'course': 3,
-    'favourite_books': ["Atomic habits", "O'tkan kunlar", "Dunyoning ishlari"],
-    'is_completed': False,
-    'is_married': True
-}
+# student = {
+#     'fullname': "John Doe",
+#     'age': 20,
+#     'course': 3,
+#     'favourite_books': ["Atomic habits", "O'tkan kunlar", "Dunyoning ishlari"],
+#     'is_completed': False,
+#     'is_married': True
+# }
 
-# 1. Qiymatlarni olish
-print(student['fullname'])
-print(student['age'])
-print(student['favourite_books'])
+# # 1. Qiymatlarni olish
+# print(student['fullname'])
+# print(student['age'])
+# print(student['favourite_books'])
 
-for book in student['favourite_books']:
-    print(book)
+# for book in student['favourite_books']:
+#     print(book)
 
-# 2. Qiymatlarni o'zgartirish
-student['age'] = 21
-student['course'] = 4
-print(student)
+# # 2. Qiymatlarni o'zgartirish
+# student['age'] = 21
+# student['course'] = 4
+# print(student)
 
-# 3. Yangi key-value qo'shish
-student['hobbies'] = ["Reading a book", 'Watching a football match', 'Learning knowledges']
-print(student)
+# # 3. Yangi key-value qo'shish
+# student['hobbies'] = ["Reading a book", 'Watching a football match', 'Learning knowledges']
+# print(student)
 
-# 4. Key-value ni o'chirish
-del student['is_married'] 
-print(student)
+# # 4. Key-value ni o'chirish
+# del student['is_married'] 
+# print(student)
 
-# 5. Empty dict(bo'sh lug'at)
-talaba_1 = {}
+# # 5. Empty dict(bo'sh lug'at)
+# talaba_1 = {}
 
-talaba_1['ism'] = 'qobil rasulov'
-talaba_1['kurs'] = 3
-talaba_1['yosh'] = 20
-print(talaba_1)
-print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+# talaba_1['ism'] = 'Menga'
+# talaba_1['kurs'] = 3
+# talaba_1['yosh'] = 20
+# print(talaba_1)
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
 
-# get() metodi
-telefonlar = {
-    'ali':'iphone x',
-    'vali':'galaxy s9',
-    'olim':'mi 10 pro',
-    'orif':'nokia 3310'
-}
+# # get() metodi
+# telefonlar = {
+#     'ali':'iphone x',
+#     'vali':'galaxy s9',
+#     'olim':'mi 10 pro',
+#     'orif':'nokia 3310'
+# }
 
-print(telefonlar.get('vali')) # 'galaxy s9'
-print(telefonlar.get('akmal')) # None
+# print(telefonlar.get('vali')) # 'galaxy s9'
+# print(telefonlar.get('akmal')) # None
+
+
+
+
+
