1b499baf413625d6f20395f4d0d65f3d99027b4c
xursandbekjs-maker <xursandbekjumaboyev987@gmail.com>
2026-03-26 16:25:29 +0500

    refactor: update salom_ber function to accept a parameter and improve usage examples

--- function.py
+++ function.py
@@ -1,8 +1,24 @@
 # Function - ma'lum bir vazifani bajaradigan kod blokidir. 
 # Ularni qayta ishlatish mumkin va ular dastur tuzilishini yaxshilaydi.
 
-def salom_ber():
-    print("Assalomu alaykum!")
+# def salom_ber():
+#     print("Assalomu alaykum!")
 
-salom_ber() # Funksiyani chaqirish
-salom_ber() # Funksiyani yana bir marta chaqirish
+# salom_ber() # Funksiyani chaqirish
+# salom_ber() # Funksiyani yana bir marta chaqirish
+
+# FUNKSIYAGA QIYMAT UZATISH
+# parametrlar.
+# argumentlar.
+def salom_ber(ism):
+    print(f"Assalomu alaykum, {ism}!")
+
+salom_ber("Ali") # Funksiyabi chaqirish 
+salom_ber("Vali")
+salom_ber("Akmal")
+
+def yigindi(a, b):
+    print(a + b)
+
+yigindi(5, 10) # 15
+yigindi(3, 7) # 10
