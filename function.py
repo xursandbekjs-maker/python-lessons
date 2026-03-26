e2aad4cd4decbced4f94626734c6ebc791886c7a
xursandbekjs-maker <xursandbekjumaboyev987@gmail.com>
2026-03-26 16:14:03 +0500

    feat: add function.py to introduce a greeting function

---
+++ function.py
@@ -0,0 +1,8 @@
+# Function - ma'lum bir vazifani bajaradigan kod blokidir. 
+# Ularni qayta ishlatish mumkin va ular dastur tuzilishini yaxshilaydi.
+
+def salom_ber():
+    print("Assalomu alaykum!")
+
+salom_ber() # Funksiyani chaqirish
+salom_ber() # Funksiyani yana bir marta chaqirish
