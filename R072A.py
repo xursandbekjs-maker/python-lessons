0a4f7f0a352f0126f174706a2dae81d770c4a63b
xursandbekjs-maker <xursandbekjumaboyev987@gmail.com>
2026-04-09 15:28:16 +0500

    feat: implement even and odd digit sum calculation with conditional output

--- R072A.py
+++ R072A.py
@@ -1,5 +1,16 @@
 N = input() # 1339 => "1339"
 print(type(N))
-
+even_sum = 0
+odd_sum = 0
 for index in range(len(N)):
-    print(index, N[index])
+    digit = int(N[index])
+    if (index + 1) % 2 == 1:
+        odd_sum += digit
+    else:
+        even_sum += digit
+        
+diff = odd_sum - even_sum
+if diff == 0 or diff % 11 == 0:
+    print("YES")
+else:    
+    print("NO")
