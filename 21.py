df7eb3b531f947f3d37e2ae087f27b249b25b86c
xursandbekjs-maker <xursandbekjumaboyev987@gmail.com>
2026-04-11 15:23:08 +0500

    feat: add new Python scripts for various calculations and functions

---
+++ 21.py
@@ -0,0 +1,3 @@
+n = int(input())
+qoldiq = n * (n + 1) * (n + 2) % 6
+print(qoldiq)
---
+++ 22.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+if a % b == 0 or b % a == 0:
+    print(True)
+else:
+    print(False)
