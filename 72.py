04482912727291184dbb473aee71f29c1af3a771
xursandbekjs-maker <xursandbekjumaboyev987@gmail.com>
2026-04-21 15:14:40 +0500

    feat: add palindrome check and new script to print numbers not divisible by 3

---
+++ 72.py
@@ -0,0 +1,4 @@
+n = int(input())
+for son in range(1, n + 1):
+    if son % 3 != 0:
+        print(son, end=" ")
---
+++ 75.py
@@ -0,0 +1,6 @@
+s = input().strip().lower()
+print(s == s[::-1])
+# if s == s[::-1]:
+#     print("True")
+# else:
+#     print("False")
