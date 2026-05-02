610538d6717d2d32cb1bc77b092be1078e4c570d
xursandbekjs-maker <xursandbekjumaboyev987@gmail.com>
2026-05-02 16:21:07 +0500

    feat: implement function to calculate sum and product of digits for numbers between 100 and 1000

--- 94.py
+++ 94.py
@@ -2,3 +2,16 @@
 # 2. raqamlar ko'paytmasini topuvchi funksiya
 # 3. 100 dan 1000 gacha bo'lgan sonlar kerak
 # har bir sonni p / s ga qoldiqsiz bo'linadigan chiqaring
+def raqamlar_yigindisi_kopaytmasi(son):
+    s, p = 0, 1 # son = 785 => "7", "8", "5"
+    for raqam in str(son):
+        s += int(raqam)
+        p *= int(raqam)
+
+    return s, p
+
+for son in range(100, 1000):
+    s, p = raqamlar_yigindisi_kopaytmasi(son)
+    if p % s == 0:
+        print(son)
+        
